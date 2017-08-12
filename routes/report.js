c8ea0bdd851e60635f4776829b09d0e809e8acb0
Brundaraju <lbrundaraju@gmail.com>
2017-08-12 20:42:13 +0530

    Update report.js

--- routes/report.js
+++ routes/report.js
@@ -13,47 +13,51 @@ var con = mysql.createConnection({
 var count;
 var depp = [];
 router.post('/report',function (req , res) {
-    function first(callback) {
-        con.query("select count(*) as count from dep", function (err, result2) {
-            if (err) throw err;
-            console.log(result2);
+    if(req.session.a===1) {
+        function first(callback) {
+            con.query("select count(*) as count from dep", function (err, result2) {
+                if (err) throw err;
+                console.log(result2);
 
-            console.log(JSON.stringify(result2));
-            var x = JSON.stringify(result2);
-            console.log(x);
-            var y = JSON.parse(x);
-            console.log(y);
-            console.log(typeof y);
+                console.log(JSON.stringify(result2));
+                var x = JSON.stringify(result2);
+                console.log(x);
+                var y = JSON.parse(x);
+                console.log(y);
+                console.log(typeof y);
 
-            count = result2[0].count;
+                count = result2[0].count;
+            });
+            con.query("select * from dep", function (err, result1) {
+                if (err) throw err;
+                console.log(result1);
+
+
+                for (var i = 0; i < count; i++) {
+                    console.log(result1[i].depname);
+                    depp[i] = result1[i].depname;
+                    console.log(depp[i]);
+                }
+                console.log(JSON.stringify(result1));
+                console.log(depp);
+
+                callback();
+
+            });
+
+        }
+
+        first(function () {
+
+            res.render('report.ejs', {
+                count: count,
+                dep: depp
+            });
+
         });
-        con.query("select * from dep", function (err, result1) {
-            if (err) throw err;
-            console.log(result1);
-
-
-            for (var i = 0; i < count; i++) {
-                console.log(result1[i].depname);
-                depp[i] = result1[i].depname;
-                console.log(depp[i]);
-            }
-            console.log(JSON.stringify(result1));
-            console.log(depp);
-
-            callback();
-
-        });
-
     }
-
-    first(function () {
-
-        res.render('report.ejs', {
-                    count: count,
-                    dep: depp
-        });
-
-    });
+    else
+        res.render('admin.ejs');
 });
 
 module.exports = router;
