dc8ea7f33af5459d0387af7d02ea6118ed80d13d
Brundaraju <lbrundaraju@gmail.com>
2017-10-06 20:18:15 +0530

    Update sendmail.js

--- routes/sendmail.js
+++ routes/sendmail.js
@@ -5,7 +5,7 @@ var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "mysql12345XXX",
-    database:"project",
+    database:"questionnaire",
     dateStrings : 'date'
 });
 
